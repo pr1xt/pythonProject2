6e1d1b1a2f5666f5c53c3d3613f30b9a2a44d780
pr1xt <timurkn2006@gmail.com>
2023-01-07 21:23:44 +0300

    2 Commit

--- codee.py
+++ codee.py
@@ -1,16 +1,12 @@
-
 for x in range(1, 11):
     print('{0:2d}{1:4d}{2:5d}'.format(x, x*x, x*x*x)) # d - это типо отступ
-print("", end=" 2323 ")
-print("never gonna give u up")
-d=(input("КАК там с деньгами?"))
-f = open('C:\Programming\pythonProject2\\filed.txt', 'r', encoding="utf-8")#Открыть+читать
+d=(input("Что записать в файл?"))
+k = int(input("Сколько раз вписать?"))
+#f = open('C:\Programming\pythonProject2\\filed.txt', 'r', encoding="utf-8")#Открыть+читать
 m = open('C:\Programming\pythonProject2\\Wfiled.txt', 'w', encoding="utf-8")#создать+открыть
-m.write("")# запись в файл
-print(f.read())
-### 100 РАЗ ПИШЕМ 11 01  В ОДИН ФАЙЛ
-
-for x in range(1, 101):
+#m.write("")# запись в файл
+#print(f.read()) - вывод в консоль содержимого файла файлед.тхт
+### k РАЗ ПИШЕМ d  В ОДИН ФАЙЛ
+for x in range(0, k):
     m = open('C:\Programming\pythonProject2\\Wfiled.txt', 'a', encoding="utf-8")  # создать+открыть
     m.writelines("  "+d+"\n")
-print(d)
