9bdb4b4c464eb5d157a3a684d31664fe67be4431
pr1xt <timurkn2006@gmail.com>
2023-03-24 13:11:26 +0100

    2 version of basic calc

--- calc_beta.py
+++ calc_beta.py
@@ -6,45 +6,48 @@ num = list(num)
 
 for x in range(len(num)):
     if num[x] == "+":
-        num[x] = "0.1"
+        num[x] = "-0.11"
     if num[x] == "-":
         num[x] = "-0.1"
     if num[x] == "*":
-        num[x] = "0.2"
+        num[x] = "-0.22"
     if num[x] == "/":
         num[x] = "-0.2"
 
 while True:
     if i == len(num) - 1:
         break
-    if float(num[i]) >= 1 and float(num[i+1]) >= 1:
+    if num[i] == "." or num[i+1] == ".":
+        i += 1
+        continue
+    if float(num[i]) >= 0 and float(num[i+1]) >= 0:
         num[i] = num[i] + num[i+1]
         num.pop(i+1)
         i -= 1
     i += 1
-#print(num)
+print(num)
 i = 0
 
 while True:
     if i == len(num):
         break
-    if num[i] == "0.1":
-        num[i-1] = int(int(num[i-1]) + int(num[i+1]))
+    if num[i] == "-0.11":
+        num[i-1] = float(float(num[i-1]) + float(num[i+1]))
         num.pop(i)
         num.pop(i)
         i -= 1
     if num[i] == "-0.1":
-        num[i-1] = int(int(num[i-1]) - int(num[i+1]))
+        num[i-1] = float(float(num[i-1]) - float(num[i+1]))
         num.pop(i)
         num.pop(i)
         i -= 1
-    if num[i] == "0.2":
-        num[i-1] = int(int(num[i-1]) * int(num[i+1]))
+    if num[i] == "-0.22":
+        num[i-1] = float(float(num[i-1]) * float(num[i+1]))
         num.pop(i)
         num.pop(i)
         i -= 1
     if num[i] == "-0.2":
-        num[i-1] = int(int(num[i-1]) / int(num[i+1]))
+        num[i-1] = float(float(num[i-1]) / float(num[i+1]))
         num.pop(i)
         num.pop(i)
         i -= 1
